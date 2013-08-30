6e0c6bef3b287e454e162002601e68532fc68369
Michael Fagan <mfagan@gmail.com>
2013-08-30 12:02:22 -0400

    outputing env so we can see what we have; adding another controller action to demonstrate handling exceptions

--- phoney/app/controllers/phoney_controller.rb
+++ phoney/app/controllers/phoney_controller.rb
@@ -1,5 +1,15 @@
 class PhoneyController < Tracks::Controller
+
   def a_phone
-    "Rotary!"
+    "raw environment info:\n<pre>\n#{env}\n</pre>"
   end
+
+  def bad_method
+    begin
+      raise "oh noes!"
+    rescue
+      "An error has occured.<br />Sorry!"
+    end
+  end
+
 end
