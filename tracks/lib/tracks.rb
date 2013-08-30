3abb2a1cc582587c4fd5c5424669c73eec16998e
Michael Fagan <mfagan@gmail.com>
2013-08-30 11:52:30 -0400

    return 404 for favicon.ico instead of having an exception

--- tracks/lib/tracks.rb
+++ tracks/lib/tracks.rb
@@ -5,12 +5,18 @@ require "tracks/string"
 module Tracks
 
   class Application
+
     def call(env)
+
+      return [404, {'Content-Type' => 'text/html'}, []] if env['PATH_INFO'] == '/favicon.ico'
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
       [200, {'Content-Type' => 'text/html'}, [text]]
+
     end
+
   end
 
   class Controller
