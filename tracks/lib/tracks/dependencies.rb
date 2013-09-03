1c8d00d9f34189115cb80c1e660bda82ae3f3bd5
Michael Fagan <mfagan@gmail.com>
2013-09-03 10:22:25 -0400

    improvement to const_missing method that prevents calling it infinitely

--- tracks/lib/tracks/dependencies.rb
+++ tracks/lib/tracks/dependencies.rb
@@ -1,6 +1,9 @@
 class Object
   def self.const_missing(c)
+    return nil if @in_const_missing
+    @in_const_missing = true
     require Tracks.to_underscore(c.to_s)
     Object.const_get(c)
+    @in_const_missing = true
   end
 end
