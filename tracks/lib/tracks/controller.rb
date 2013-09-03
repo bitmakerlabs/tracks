a1fa39a8c02afa316ac01f74fa2e5a0f0a96debc
Michael Fagan <mfagan@gmail.com>
2013-09-03 12:27:29 -0400

    moving views into separate folders for each controller

--- tracks/lib/tracks/controller.rb
+++ tracks/lib/tracks/controller.rb
@@ -12,8 +12,13 @@ module Tracks
       @env
     end
 
+    def controller_name
+      Tracks.to_underscore( self.class.to_s.gsub(/Controller$/, '') )
+    end
+
     def render(view_name, locals = {})
-      template = File.read( File.join("app", "views", "#{view_name}.html.erb") )
+      filename = File.join("app", "views", controller_name, "#{view_name}.html.erb")
+      template = File.read(filename)
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(:env => env)
     end
