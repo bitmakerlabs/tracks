732f38269a3f50fe70c93668f2153098f5a18eb6
Michael Fagan <mfagan@gmail.com>
2013-09-04 13:07:46 -0400

    adding methods to sqlite model eg to create records

--- tracks/lib/tracks/sqlite_model.rb
+++ tracks/lib/tracks/sqlite_model.rb
@@ -8,9 +8,46 @@ module Tracks
   module Model
 
     class SQLite
+
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+        when Numeric
+          val.to_s
+        when String
+          "'#{val}'"
+        else
+          raise "Can't change #{val.class} to SQL"
+        end
+      end
+
+     def self.create(values)
+       values.delete 'id'
+       keys = schema.keys - ['id']
+       vals = keys.map do |key|
+         values[key] ? to_sql(values[key]) : "null"
+       end
+       DB.execute <<SQL
+         INSERT INTO #{table} (#{keys.join(', ')}) VALUES (#{vals.join(', ')});
+SQL
+       data = Hash[keys.zip vals]
+       sql = "SELECT last_insert_rowid();"
+       self.new data
+     end
+
+     def self.count
+       DB.execute(<<SQL)[0][0]
+         SELECT COUNT(*) FROM #{table}
+SQL
+     end
+
       def self.table
         Tracks.to_underscore name
       end
+
       def self.schema
         return @schema if @schema
         @schema = {}
@@ -19,6 +56,7 @@ module Tracks
         end
         @schema
       end
+
     end
 
   end
