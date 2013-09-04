9bc8faf1f6e313971fbae6bc56583d2938f9426a
Michael Fagan <mfagan@gmail.com>
2013-09-04 16:32:54 -0400

    improving dynamic finders for sqlite model in example app (not in framework)

--- phoney/app/controllers/blog_controller.rb
+++ phoney/app/controllers/blog_controller.rb
@@ -1,10 +1,16 @@
 class MyTable < Tracks::Model::SQLite
-  ['title', 'body'].each do |method|
-    define_method(method) {
-      self[method]
-    }
+
+  def method_missing(method, *args)
+    # todo: figure out why define_method appears to not be working here. if you call .title twice, it seems does the puts below twice
+    method_name = method.to_s
+    return false unless ['title', 'body'].include? method_name
+    define_method(method) { @hash[method_name] }
+    puts "SHOULD ONLY SEE THIS ONCE"
+    @hash[method_name] # should be able to just do self.send method
   end
+
 end
+
 class BlogController < Tracks::Controller
 
   def show
