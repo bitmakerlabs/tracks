89623df07ada7f2dbe6da84816dae093354f2723
Michael Fagan <mfagan@gmail.com>
2013-09-03 20:49:19 -0400

    adding an add method for models and using it in the app in a hardcoded way

--- phoney/app/controllers/phoney_controller.rb
+++ phoney/app/controllers/phoney_controller.rb
@@ -1,8 +1,17 @@
 class PhoneyController < Tracks::Controller
 
+  # 'real' actions we will be keeping
+
   def index
     render :index, :phones => FileModel.all
   end
+
+  def new_phone
+    phone = FileModel.create( {'phone' => 'cellular', 'inventor' => 'motorola'} )
+    render :phone, :obj => phone
+  end
+
+  # actions used when developing, but won't be kept
 
   def a_phone
     render :a_phone, :noun => 'battery'
--- tracks/lib/tracks/file_model.rb
+++ tracks/lib/tracks/file_model.rb
@@ -34,6 +34,22 @@ module Tracks
         files.map { |f| FileModel.new f }
       end
 
+      def self.create(attrs)
+        hash = {
+          'phone' => attrs['phone'] || '',
+          'inventor' => attrs['inventor'] || ''
+        }
+        existing_file_names = Dir['db/phones/*.json'].map { |f| f.split('/')[-1] }
+        id = existing_file_names.map { |n| n[0..-5].to_i }.max + 1
+        new_filepath = "db/phones/#{id}.json"
+        File.open(new_filepath, "w") do |f|
+          f.write <<TEMPLATE
+            { "phone": "#{hash['phone']}", "inventor": "#{hash['inventor']}"}
+TEMPLATE
+        end
+        FileModel.new new_filepath
+      end
+
     end
 
   end
